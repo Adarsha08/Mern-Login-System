9c58892792dcc23a1339b75fd5fcdcb27823b0a1
Adarsha Gautam <gautamadarsha08@gmail.com>
2025-08-31 11:57:10 +0545

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+import './App.css'
+import Login from './Login'
+import Signup from './signup'
+import {BrowserRouter , Route, Routes} from 'react-router-dom'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <> 
+      <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Signup.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+
+const Signup = () => {
+ const [name ,setname]=useState("");
+ const [email ,setemail]=useState("");
+ const [password ,setpassword]=useState("");
+ const handlechanage=(e)=>
+ {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(password);
+ }
+  return (
+       <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-lg">
+        <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
+
+        <form onSubmit={handlechanage} className="space-y-4">
+          {/* Name Input */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Name
+            </label>
+            <input
+             onChange={(e)=>setname(e.target.value)}
+              type="text"
+              placeholder="Enter your name"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+           
+          </div>
+
+          {/* Email Input */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email
+            </label>
+            <input
+             onChange={(e)=>setemail(e.target.value)}
+              type="email"
+              placeholder="Enter your email"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Password Input */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Password
+            </label>
+            <input
+             onChange={(e)=>setpassword(e.target.value)}
+              type="password"
+              placeholder="Enter your password"
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Register Button */}
+          <button
+            type="submit"
+            className=" cursor-pointer w-full py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
+          >
+            Register
+          </button>
+
+          {/* Already have account text */}
+          <p className="text-sm text-center text-gray-600">Already have an account?</p>
+
+          {/* Login Button */}
+          <Link to="/login"
+            type="button"
+             className="block w-full py-2 border border-green-500 text-green-600 rounded-lg hover:bg-green-50 transition text-center"
+>
+            Login
+          </Link>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
